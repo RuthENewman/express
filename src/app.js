57bb03ad0a8cc2bd9194e69a3c012c516a9ceb07
Ruth Newman <ruthnewman@gmail.com>
2019-06-07 16:03:09 +0100

    added header and footer partials

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 app.use(express.static(publicDirPath))
 
 app.get('', (request, response) => {
@@ -17,12 +23,14 @@ app.get('', (request, response) => {
 app.get('/about', (request, response) => {
   response.render('about', {
     title: 'About Me',
-    creator: 'Ruth Newman'
+    name: 'Ruth Newman'
   })
 })
 
 app.get('/faq', (request, response) => {
   response.render('faq', {
+    title: 'Frequently Asked Questions',
+    name: 'Ruth Newman',
     message: 'Answers to the most common queries'
   })
 })
