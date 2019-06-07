29186e4470d1f55fea2883a062e9e330f1b09580
Ruth Newman <ruthnewman@gmail.com>
2019-06-07 01:06:23 +0100

    with handlebars views

--- src/app.js
+++ src/app.js
@@ -4,7 +4,28 @@ const express = require('express');
 const app = express()
 const publicDirPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirPath))
+
+app.get('', (request, response) => {
+  response.render('index', {
+    title: 'Clima Info',
+    name: 'Ruth Newman'
+  })
+})
+
+app.get('/about', (request, response) => {
+  response.render('about', {
+    title: 'About Me',
+    creator: 'Ruth Newman'
+  })
+})
+
+app.get('/faq', (request, response) => {
+  response.render('faq', {
+    message: 'Answers to the most common queries'
+  })
+})
 
 app.get('/weather', (request, response) => {
     response.send({
